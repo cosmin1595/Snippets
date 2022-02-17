0b6dc1cb252eed926af5daa7147e7600d5e80e17
cosmin1595 <cosmin1595@gmail.com>
2022-02-17 17:22:26 +0200

    Create WpfEnumConverters.cs

---
+++ WpfEnumConverters.cs
@@ -0,0 +1,69 @@
+    public class ListEnumDescriptionConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            List<string> list = new List<string>();
+            var valueList = (value as IEnumerable<MarkerModel>);//.Cast<MarkerModel>().ToList();
+
+            if (value != null)
+            {
+                if (valueList.Count() > 0)
+                {
+                    foreach (var item in valueList)
+                    {
+                        Enum myEnum = (Enum)item;
+                        string description = myEnum.GetDisplayAttribute();
+                        //string description = myEnum.GetDescription();
+                        list.Add(description);
+                    }
+                }
+            }
+            //Enum myEnum = (Enum)value;
+            //string description = myEnum.GetDisplayAttribute();
+            //string description = myEnum.GetDescription();
+            return list;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MarkerEnumDescriptionConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Enum myEnum = (Enum)value;
+            string description = myEnum.GetDisplayAttribute();
+            //string description = myEnum.GetDescription();
+            return description;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //return source.GetType().GetMember(source.ToString())
+            //             .FirstOrDefault()?
+            //             .GetCustomAttribute<DisplayAttribute>()
+            //             .GetDescription() ?? "unknown";
+            object ret = MarkerModel.WAYPOINT_ANCHOR_2_CT;
+            if(value == null) { return ret; }
+
+            FieldInfo[] fi = targetType.GetFields();
+            for(int i =0; i < fi.Length; i++)
+            {
+                DisplayAttribute[] attr = (DisplayAttribute[])fi[i].GetCustomAttributes(
+                    typeof(DisplayAttribute), false);
+                if(attr != null && attr.Length > 0)
+                {
+                    if(value.Equals(attr[0].GetDescription()))
+                    {
+                        ret = targetType.GetField(fi[i].Name);
+                        break;
+                    }
+                }
+            }
+            return ret;
+            //throw new NotImplementedException();
+        }
+    }
